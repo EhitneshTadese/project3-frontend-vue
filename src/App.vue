b1081791a5d73eaf7cb76ac2122992a9be37e621
lacebx <arsenemanzi@pm.me>
2024-12-05 01:50:35 -0500

    edited the navbar

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,31 @@
 <template>
     <v-app>
-      <v-app-bar v-if="!isLoginPage">
-            <v-img
-                class="mx-2"
-                :src="logo"
-                max-height="40"
-                max-width="40"
-                contain
-            ></v-img>
-            <v-app-bar-title  >Resume Management Application</v-app-bar-title>
+        <v-app-bar v-if="!isLoginPage">
+          <v-btn @click="goCreateResume" class="d-inline-flex justify-center align-center" height="auto" width="auto" style="padding: 0;">
+  <v-img
+    class="mx-2"
+    :src="logo"
+    height="90"
+    width="90"
+    contain
+  ></v-img>
+</v-btn>
+
+            <v-app-bar-title>Resume Builder</v-app-bar-title>
             <v-spacer></v-spacer>
             <v-toolbar-items>
-                <v-btn 
-                    variant="text"
-                    @click="goList"
-                    >
-                  List
-                </v-btn>
-                <v-btn 
-                    variant="text"
-                    @click="goAccount"
-                    >
-                  <v-icon left>mdi-account</v-icon>
-                  Account
+                <v-btn variant="text" @click="goTemplates">Templates</v-btn>
+                <v-btn variant="text" @click="goAccount">
+                    <v-icon left>mdi-account</v-icon>
+                    Account
                 </v-btn>
             </v-toolbar-items>
-      </v-app-bar>
-      <v-main >
-        <v-container>
-          <router-view />
-        </v-container>
-      </v-main>  
+        </v-app-bar>
+        <v-main>
+            <v-container>
+                <router-view />
+            </v-container>
+        </v-main>
     </v-app>
 </template>
 
